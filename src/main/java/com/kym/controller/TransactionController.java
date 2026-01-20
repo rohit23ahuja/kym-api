585d13497665e1ca5de2adf0bd211453ba93306e
Rohit Ahuja <rohit23ahuja@gmail.com>
2026-01-20 19:36:52 +0530

    allowing cors

--- src/main/java/com/kym/controller/TransactionController.java
+++ src/main/java/com/kym/controller/TransactionController.java
@@ -3,19 +3,19 @@ package com.kym.controller;
 import com.kym.model.CreditCardTransaction;
 import com.kym.repository.CreditCardTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class TransactionController {
 
     @Autowired
     private CreditCardTransactionRepository creditCardTransactionRepository;
 
     @GetMapping(path = "/transactions")
-    public Iterable<CreditCardTransaction> getTransactions(){
+    public Iterable<CreditCardTransaction> getTransactions() {
         return creditCardTransactionRepository.findAll();
     }
 }
